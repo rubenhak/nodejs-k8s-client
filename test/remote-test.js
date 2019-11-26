25130ffd4806c8c7d4099b4585100d7d74a46c1e
Ruben Hakopian <ruben.hakopian@gmail.com>
2019-11-25 17:56:46 -0800

    Added mocha test holders
    Modified Areas:
    package-lock.json
    
    Files Changed:
    M  package-lock.json
    M  package.json
    A  test/remote-test.js

---
+++ test/remote-test.js
@@ -0,0 +1,13 @@
+var should = require('should');
+
+var Index = require('../index');
+
+describe('remote-test.js', function() {
+
+    it('case-01', function () {
+        
+        return true;
+
+    });
+
+});
