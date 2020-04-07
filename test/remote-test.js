4eaec3defe5030fce25d30ae52cd80805f54e266
Ruben Hakopian <ruben.hakopian@gmail.com>
2020-04-06 19:09:09 -0700

    Added watch test. Implemented watch stop.

--- test/remote-test.js
+++ test/remote-test.js
@@ -1,3 +1,4 @@
+var Promise = require('the-promise');
 var should = require('should');
 
 var ClientFetcher = require('./client');
@@ -17,6 +18,45 @@ describe('gcp-tests', function() {
 
                 result.should.matchEvery(x => x.metadata.namespace == "kube-system");
                 result.should.matchAny(x => x.metadata.name == "kube-dns");
+            });
+
+    });
+
+    it('deployment-watch', function () {
+        this.timeout(10*1000);
+
+        return ClientFetcher()
+            .then(client => {
+
+                var watchResult = {
+                    connected: false,
+                    disconnected: false,
+                    result: []
+                }
+
+                var watch = client.Deployment.watchAll("kube-system", (action, data) => {
+                    if (action == "ADDED") {
+                        watchResult.result.push(data);
+                    }
+                }, () => {
+                    watchResult.connected = true;
+                }, (resourceAccessor, data) => {
+                    watchResult.disconnected = true;
+                })
+
+                return Promise.timeout(5000)
+                    .then(() => {
+                        watch.stop();
+                        return watchResult;
+                    });
+            })
+            .then(watchResult => {
+                watchResult.result.should.be.an.Array;
+
+                console.log(watchResult.result.map(x => x.metadata.name));
+
+                watchResult.result.should.matchEvery(x => x.metadata.namespace == "kube-system");
+                watchResult.result.should.matchAny(x => x.metadata.name == "kube-dns");
             });
             
     });
