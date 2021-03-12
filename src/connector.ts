35628896db414c58035d361d18633013c7a291f2
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-03-12 01:02:54 -0800

    fixes to local connector

--- src/connector.ts
+++ src/connector.ts
@@ -2,17 +2,19 @@ import { ILogger } from 'the-logger';
 import * as fs from 'fs';
 import dotenv from 'dotenv'
 
-import { KubernetesClient } from './client';
+import { KubernetesClient, KubernetesClientConfig } from './client';
 
 dotenv.config()
 
-export function connectFromPod(logger : ILogger) : KubernetesClient
+export function connectFromPod(logger : ILogger) : Promise<KubernetesClient>
 {
-    var k8sConfig = {
+    const k8sConfig : KubernetesClientConfig = {
         server: 'https://' + process.env.KUBERNETES_SERVICE_HOST + ':' + process.env.KUBERNETES_SERVICE_PORT_HTTPS,
         token: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/token', 'utf8'),
-        caData: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt', 'utf8')
+        httpAgent: {
+            ca: fs.readFileSync('/var/run/secrets/kubernetes.io/serviceaccount/ca.crt', 'utf8')
+        }
     };
-    var client = new KubernetesClient(logger, k8sConfig);
-    return client;
+    const client = new KubernetesClient(logger, k8sConfig);
+    return client.init();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export { KubernetesError } from './types'
+export { KubernetesError, KubernetesObject } from './types'
 
 export { connectFromPod } from './connector';
 
 export { KubernetesClient, KubernetesClientConfig } from './client'
+export { ResourceAccessor } from './resource-accessor'
 export { WatchCallback, ConnectCallback, DisconnectCallback, DeltaAction } from './resource-watch'
